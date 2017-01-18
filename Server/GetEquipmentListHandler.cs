2d4a46d3026e1228fb64fdf2d3345356b0d4a9cc
Hendrik Nigul <hendrik.nigul@gmail.com>
2017-01-18 11:28:00 +0200

    Equipment list usecase

---
+++ Server/GetEquipmentListHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Logging;
+using Shared;
+
+
+namespace Server
+{
+    public class GetEquipmentListHandler :
+        IHandleMessages<GetEquipmentList>
+    {
+        static ILog log = LogManager.GetLogger<GetEquipmentListHandler>();
+
+        public Task Handle(GetEquipmentList message, IMessageHandlerContext context)
+        {
+            log.Info("GetEquipmentList handling");
+
+            EquipmentList equipments = new EquipmentList();
+
+            equipments.Add(new Equipment() { 
+                Name = "Caterpillar bulldozer", Type = EquipmentType.Heavy, 
+                Id = 0, Url = "http://s7d2.scene7.com/is/image/Caterpillar/C10337180" });
+
+
+            return context.Reply(equipments);
+        }
+    }
+}
---
+++ Shared/Messages/Equipment.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NServiceBus;
+
+namespace Shared
+{
+    public enum EquipmentType 
+    {
+        Heavy, Regular, Specialized
+    }
+
+    [Serializable]
+    public class Equipment
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public EquipmentType Type { get; set; }
+        public string Url { get; set; }
+    }
+
+    [Serializable]
+    public class EquipmentList : List<Equipment>
+    {
+
+    }
+
+    public class GetEquipmentList : IMessage
+    {
+
+    }
+}
